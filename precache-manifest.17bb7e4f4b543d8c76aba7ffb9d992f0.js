43e76f2d3567e0c4ad540fff339a357bd5f60bd4
Vladimir <33632745+Omhet@users.noreply.github.com>
2019-03-30 16:41:58 +0300

    Add files via upload

---
+++ precache-manifest.17bb7e4f4b543d8c76aba7ffb9d992f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0562eef2ce4c949c81c1",
+    "url": "/static/css/main.341decd3.chunk.css"
+  },
+  {
+    "revision": "0562eef2ce4c949c81c1",
+    "url": "/static/js/main.3d6d4c7c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "392e1f6af1181bcbd3b9",
+    "url": "/static/js/2.1fe605be.chunk.js"
+  },
+  {
+    "revision": "627ae3351dfe0890d5173ed3263c1041",
+    "url": "/index.html"
+  }
+];
