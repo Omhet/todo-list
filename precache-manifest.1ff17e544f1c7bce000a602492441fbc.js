213eaadb5a9fe650d66275e5241d64fd9f899e0b
Omhet <omhetehmo@yandex.ru>
2019-03-31 22:05:23 +0400

    Updates

---
+++ precache-manifest.1ff17e544f1c7bce000a602492441fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b92a5c77d2b796e3d4b",
+    "url": "/todo-list/static/css/main.341decd3.chunk.css"
+  },
+  {
+    "revision": "2b92a5c77d2b796e3d4b",
+    "url": "/todo-list/static/js/main.94ec361d.chunk.js"
+  },
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.714397e2.js"
+  },
+  {
+    "revision": "392e1f6af1181bcbd3b9",
+    "url": "/todo-list/static/js/2.1fe605be.chunk.js"
+  },
+  {
+    "revision": "12f932100164926f5413c866dc05f674",
+    "url": "/todo-list/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63e773417afc80b882eb45c04b717fea.js"
+  "/todo-list/precache-manifest.1ff17e544f1c7bce000a602492441fbc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todo-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
